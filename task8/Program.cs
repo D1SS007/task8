33d1537731b83ff4c62391852d92bb416c6d6f43
D1SS <D1SS@DESKTOP-2QR7GQG>
2022-03-18 18:50:53 +0300

    Добавьте файлы проекта.

---
+++ task8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int receptionTime = 10;
+            int queueTime;
+            int fullHours;
+            int minutes;
+
+
+            Console.WriteLine("Введите количество старушек ");          
+
+            int amountOfPeople = Convert.ToInt32(Console.ReadLine());
+
+            queueTime = receptionTime * amountOfPeople;
+
+            fullHours = queueTime / 60;
+            minutes = queueTime  % 60;
+
+            Console.WriteLine("Время в очереди: " + fullHours + " часов, " + minutes + " минут " );
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
